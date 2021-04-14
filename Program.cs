f5340437798e72352c68fabcda75e43a708bbebb
felixsaid <kiambafelix@yahoo.com>
2021-04-14 20:42:39 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinearSearch
+{
+    public static class Program
+    {
+        
+        public static int? LinearSearch(int[] data, int value)
+        {
+            foreach(int item in data)
+            {
+                if(item == value)
+                {
+                    return data[item];
+                }
+
+            }
+
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] data = new int[] { 1, 4, 6, 4, 9, 9, 4, 5, 3 };
+
+            int? pos = LinearSearch(data, 6);
+
+            Console.WriteLine(pos);
+        }
+    }
+}
